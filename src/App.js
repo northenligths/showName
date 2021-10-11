cb9c7342ed1f8ab463ef2c80d8615c3a3f9411fe
akash234git <uic,17bca1245@gmail.com>
2021-10-12 01:37:13 +0530

    Added summarytwo page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Homepage from "./components/homepage/Homepage";
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import SummaryOne from "./pages/summaryOne/SummaryOne";
+import SummaryTwo from "./pages/summaryTwo/SummaryTwo";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
       <Route path="/" exact component={Homepage}/>
       <Route path="/SummaryOne" component={SummaryOne} />
+      <Route path="/SummaryTwo" component={SummaryTwo} />
      </Switch>
     </div>
     </Router>
--- src/pages/summaryTwo/SummaryTwo.js
+++ src/pages/summaryTwo/SummaryTwo.js
@@ -1,9 +1,9 @@
 import {React,useState,useEffect} from 'react'
 import axios from 'axios'
-import './summaryOne.css'
+import './summaryTwo.css'
 import { Link } from 'react-router-dom';
 
-export default function SummaryOne() {
+export default function SummaryTwo() {
     const url='https://api.tvmaze.com/search/shows?q=all'
         const [shows,setShow]=useState(null)
          useEffect(()=>{
@@ -17,18 +17,18 @@ export default function SummaryOne() {
     return (
         <div>
             <h1 className="sum">Summary</h1>
-            <div className="img-one">
-            <img src={shows[0].show.image.medium} alt="" />
+            <div className="img-two">
+            <img src={shows[1].show.image.medium} alt="" />
             </div>
             <div className="link">
             <Link className="sum-link" to="/">Book Show</Link>
             </div>
-            <div className="sum-one">
-             <div className="name"> <strong>Name</strong>-{shows[0].show.name}</div>
-              <div className="lang"><strong>Language</strong>-{shows[0].show.language}</div>
-              <div className="rating"><strong>Rating</strong>-Average({shows[0].show.rating.average})</div>
+            <div className="sum-two">
+             <div className="name"> <strong>Name</strong>-{shows[1].show.name}</div>
+              <div className="lang"><strong>Language</strong>-{shows[1].show.language}</div>
+              <div className="rating"><strong>Rating</strong>-Average({shows[1].show.rating.average})</div>
               <div className="summary"><strong>Summary</strong></div>
-              <div className="text">{shows[0].show.summary}</div>
+              <div className="text">{shows[1].show.summary}</div>
             </div>
            
 
