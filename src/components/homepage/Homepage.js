891d7a32f14e03a8d8ad20d9125746f8ac3860d9
akash234git <uic,17bca1245@gmail.com>
2021-10-11 23:14:19 +0530

    Added styling to homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -6,7 +6,7 @@ export default function Homepage(props) {
     return (
         <div>
             <div className="cards-class">
-        <h1 className="text">TV Shows</h1>
+        <h1 className="text">TV Maze</h1>
         <p className="text">Select from your favorite TV Shows</p>
         <Container>
         <Row>
@@ -28,6 +28,9 @@ export default function Homepage(props) {
       <Card>
         <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
         <CardBody>
+        <CardTitle tag="h5">All American</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
+          <CardText>Rating-Average(7.8)</CardText>
         <Link className="link-class" to="/summary">Select Show</Link>
         </CardBody>
       </Card>
@@ -36,6 +39,9 @@ export default function Homepage(props) {
       <Card>
         <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
         <CardBody>
+        <CardTitle tag="h5">All American</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
+          <CardText>Rating-Average(7.8)</CardText>
         <Link className="link-class" to="/summary">Select Show</Link>
         </CardBody>
       </Card>
@@ -44,6 +50,9 @@ export default function Homepage(props) {
       <Card>
         <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
         <CardBody>
+        <CardTitle tag="h5">All American</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
+          <CardText>Rating-Average(7.8)</CardText>
         <Link className="link-class" to="/summary">Select Show</Link>
         </CardBody>
       </Card>
