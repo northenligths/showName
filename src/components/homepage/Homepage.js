fd91150edc2313e7babec9ee614c1535a7e30ad9
akash234git <uic,17bca1245@gmail.com>
2021-10-12 00:35:25 +0530

    Fetched and displayed data on homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 import {Card, CardImg, CardBody,Row,Col,Container,CardSubtitle,CardText,CardTitle} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './homepage.css';
+import axios from 'axios'
+
+
 export default function Homepage(props) {
+            
+        const url='https://api.tvmaze.com/search/shows?q=all'
+        const [shows,setShow]=useState(null)
+         useEffect(()=>{
+            axios.get(url)
+            .then(response=>{
+                setShow(response.data)
+            })
+         },[url])
+         if(shows){
     return (
         <div>
             <div className="cards-class">
         <h1 className="text">TV Maze</h1>
-        <p className="text">Select from your favorite TV Shows</p>
+        <p className="text">Select from your favorite Movies</p>
         <Container>
         <Row>
             <Col md={3}> 
@@ -16,44 +29,44 @@ export default function Homepage(props) {
         <CardImg className="height-img" id="zoom-in"top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
         
         <CardBody>  
-        <CardTitle tag="h5">All American</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
-          <CardText>Rating-Average(7.8)</CardText>
-                    <Link className="link-class" to="/summary">Select Show</Link>
+        <CardTitle tag="h5">{shows[0].show.name}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Language-{shows[0].show.language}</CardSubtitle>
+          <CardText>Rating-Average({shows[0].show.rating.average})</CardText>
+                    <Link className="link-class" to="/summary">View Summary</Link>
         </CardBody>
         </div>
       </Card>
       </Col>
       <Col md={3}> 
       <Card>
-        <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
+        <CardImg className="height-img" top width="100%" src={shows[1].show.image.medium} alt="Card image cap" />
         <CardBody>
-        <CardTitle tag="h5">All American</CardTitle>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
-          <CardText>Rating-Average(7.8)</CardText>
-        <Link className="link-class" to="/summary">Select Show</Link>
+        <CardTitle tag="h5">{shows[1].show.name}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-{shows[1].show.language}</CardSubtitle>
+          <CardText>Rating-Good({shows[1].show.rating.average})</CardText>
+        <Link className="link-class" to="/summary">View Summary</Link>
         </CardBody>
       </Card>
       </Col>
       <Col md={3}> 
       <Card>
-        <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
+        <CardImg className="height-img" top width="100%" src={shows[2].show.image.medium} alt="Card image cap" />
         <CardBody>
-        <CardTitle tag="h5">All American</CardTitle>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
-          <CardText>Rating-Average(7.8)</CardText>
-        <Link className="link-class" to="/summary">Select Show</Link>
+        <CardTitle tag="h5">{shows[2].show.name}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-{shows[2].show.language}</CardSubtitle>
+          <CardText>Rating-Good({shows[2].show.rating.average})</CardText>
+        <Link className="link-class" to="/summary">View Summary</Link>
         </CardBody>
       </Card>
       </Col>
       <Col md={3}> 
       <Card>
-        <CardImg className="height-img" top width="100%" src="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg" alt="Card image cap" />
+        <CardImg className="height-img" top width="100%" src={shows[3].show.image.medium} alt="Card image cap" />
         <CardBody>
-        <CardTitle tag="h5">All American</CardTitle>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-English</CardSubtitle>
-          <CardText>Rating-Average(7.8)</CardText>
-        <Link className="link-class" to="/summary">Select Show</Link>
+        <CardTitle tag="h5">{shows[3].show.name}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Language-{shows[3].show.language}</CardSubtitle>
+          <CardText>Rating-Average({shows[2].show.rating.average})</CardText>
+        <Link className="link-class" to="/summary">View Summary</Link>
         </CardBody>
       </Card>
       </Col>
@@ -63,4 +76,10 @@ export default function Homepage(props) {
     </div>
         </div>
     )
+         }
+
+         return(
+           <div></div>
+         )
 }
+
